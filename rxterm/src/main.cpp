3f3dd4df723e2eb0d45c22f4a9606137203eeeaa
Gaetano Checinski <Gaetano.Checinski@gmail.com>
2017-06-20 00:59:54 +0100

    adds reflow for strings

--- rxterm/src/main.cpp
+++ rxterm/src/main.cpp
@@ -4,6 +4,43 @@
 #include <memory>
 #include <rxterm/terminal.hpp>
 
+std::string reflow(unsigned width, std::string const& s) {
+
+  std::string content = "";
+  auto b = 0;
+
+  if (width == 0) return "";
+
+  if (s.size() <= width) { return s; }
+
+  while (b < s.size()) {
+    auto t = s.substr(b, width+1)
+      .find_last_of("\n\t.,!?:;- ");
+
+    if (t == std::string::npos) {
+      content += s.substr(b, width);
+      b+=width;
+      if( b < s.size()) {
+        content +='\n';
+       }
+    } else {
+      if (s[t] == '\n') {
+        content += s.substr(b, t);
+        b=t+1;
+      } else if (t >= width) {
+        content += s.substr(b, width)+'\n';
+        b+=width;
+      } else {
+        content += s.substr(b, t+1)+'\n';
+        b=b+t+1;
+      }
+    }
+  }
+
+
+
+  return content;
+}
 
 struct Content {
   std::string content;
@@ -26,9 +63,13 @@ int main() {
   std::cout << std::endl;
   VirtualTerminal vt;
 
-  vt = renderToTerm(vt, Content{"Hello\nWorld"});
-  std::this_thread::sleep_for(1s);
-  vt = renderToTerm(vt, Content{"and\nGood\nBye\nNew World!"});
+  auto s = reflow(4, "12345-abcde-12345-ab");
+
+
+
+  std::cout << std::endl <<s << std::endl;
+
+
 
   return 0;
 }
