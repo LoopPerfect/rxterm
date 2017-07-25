9ea8819939e2be55d0d1198d72af435e0c70f38c
Gaetano Checinski <Gaetano.Checinski@gmail.com>
2017-07-25 21:22:18 +0100

    implements minimal diff for styles

---
+++ rxterm/include/style.hpp
@@ -0,0 +1,177 @@
+#ifndef RXTERM_STYLE_HPP
+#define RXTERM_STYLE_HPP
+
+#include <string>
+
+namespace rxterm {
+
+  using std::string;
+
+enum class Font {
+  None = 0,
+  Default = 1,
+  Bold = 1 << 1, // 1
+  Faint = 1 << 2, // 2
+  Roman = 1 << 3,// not Italic
+  Italic = 1 << 4, // 3
+  NotUnderline = 1 << 5,
+  Underline = 1 << 6, //4
+  Visible = 1 << 7,
+  Hidden = 1 << 8, //8
+  Uncrossed = 1 << 9,
+  Crossed = 1 << 10, // 9
+  Inherit = 1 << 11
+};
+
+enum class Color {
+  Black = 0,
+	Red = 1,
+	Green = 2,
+	Yellow = 3,
+	Blue = 4,
+	Magenta = 5,
+	Cyan = 6,
+	White = 7,
+  Default = 9,
+  Transparent = 10,
+  Inherit = 11
+};
+
+
+
+template<class X>
+constexpr std::string composeMod(X x) {
+  return x;
+}
+
+template<class X, class...Xs>
+constexpr std::string composeMod(X x, Xs...xs) {
+  auto const r = composeMod(xs...);
+  auto const delim = (r == "") || (x == "") ? "" : ";";
+  return x + delim + r;
+}
+
+template<class X, class...Xs>
+constexpr std::string computeMod(X x, Xs...xs) {
+  auto const  r = composeMod(x, xs...);
+  return (r == "") ? "" : "\e["+r+"m";
+}
+
+template<class X>
+constexpr Font fonts (X x){
+  return x;
+}
+
+
+template<class X, class...Xs>
+constexpr Font fonts(X x, Xs...xs) {
+  return Font( ((int)x) | ((int)fonts(xs...))  );
+}
+
+
+bool has(Font x, Font y) {
+  return (((int)x) & ((int)y));
+}
+
+
+
+struct Style {
+  Color bg;
+  Color fg;
+  Font font;
+
+  constexpr Style(
+      Color bg = Color::Inherit,
+      Color fg = Color::Inherit,
+      Font font = Font::Inherit)
+    : bg{bg}, fg{fg}, font{font}
+  {}
+
+
+  string defaultMod() const {
+    return has(font, Font::Default) ? "0" : "";
+  }
+
+
+  string boldMod() const {
+    return has(font, Font::Bold) ? "1" : "";
+  }
+
+  string underlineMod() const {
+    return has(font, Font::Underline) ? "4" : "";
+  }
+
+  string faintMod() const {
+    return has(font, Font::Faint) ? "2" : "";
+  }
+
+  string italicMod() const {
+    return has(font, Font::Italic) ? "3" : "";
+  }
+
+  string hiddenMod() const {
+    return has(font, Font::Hidden) ? "8" : "";
+  }
+
+  string crossedMod() const {
+    return has(font, Font::Crossed) ? "9" : "";
+  }
+
+
+  string bgMod() const {
+    return ((int)bg<10) ? std::to_string(40 + (int)bg) : "";
+  }
+
+  string fgMod() const {
+    return ((int)fg<10) ? std::to_string(30 + (int)fg) : "";
+  }
+
+  std::string render() const {
+    return computeMod(
+        defaultMod(),
+        boldMod(),
+        underlineMod(),
+        faintMod(),
+        italicMod(),
+        hiddenMod(),
+        crossedMod(),
+        bgMod(),
+        fgMod()
+    );
+  }
+};
+
+
+Style diff(Style const& a, Style const& b=Style{} ) {
+  bool keepBG = (a.bg == b.bg);
+  bool keepFG = (a.fg == b.fg);
+  bool keepFont = (a.font == b.font);
+
+  int l = (int)a.font;
+  int r = (int)b.font;
+  int reset =  (l & ~r)? 1 : 0;
+
+  return Style {
+    keepBG ? Color::Inherit : b.bg,
+    keepFG ? Color::Inherit : b.fg,
+    keepFont ? Font::Inherit : (Font)((r&(l^r))|reset)
+  };
+}
+
+
+Style cascade(Style const& p, Style const& c=Style() ) {
+  bool inheritBG = (c.bg == Color::Inherit);
+  bool inheritFG = (c.fg == Color::Inherit);
+  bool inheritFont = (c.font == Font::Inherit);
+
+  return Style {
+    inheritBG ? p.bg : c.bg,
+    inheritFG ? p.fg : c.fg,
+    inheritFont ? p.font : c.font,
+  };
+}
+
+
+}
+
+#endif
--- rxterm/src/main.cpp
+++ rxterm/src/main.cpp
@@ -3,49 +3,47 @@
 #include <chrono>
 #include <memory>
 #include <rxterm/terminal.hpp>
+#include <rxterm/style.hpp>
 
 std::string reflow(unsigned width, std::string const& s) {
 
-  std::string content = "";
-  auto b = 0;
-
   if (width == 0) return "";
-
   if (s.size() <= width) { return s; }
 
+  std::string content = "";
+  auto b = 0;
   while (b < s.size()) {
     auto t = s.substr(b, width+1)
       .find_last_of("\n\t.,!?:;- ");
 
     if (t == std::string::npos) {
       content += s.substr(b, width);
-      b+=width;
-      if( b < s.size()) {
-        content +='\n';
-       }
+      b += width;
+      if (b < s.size()) {
+        content += '\n';
+      }
     } else {
       if (s[t] == '\n') {
         content += s.substr(b, t);
-        b=t+1;
+        b = t+1;
       } else if (t >= width) {
-        content += s.substr(b, width)+'\n';
-        b+=width;
+        content += s.substr(b, width) + '\n';
+        b += width;
       } else {
-        content += s.substr(b, t+1)+'\n';
-        b=b+t+1;
+        content += s.substr(b, t+1) + '\n';
+        b += t+1;
       }
     }
   }
-
-
 
   return content;
 }
 
 struct Content {
   std::string content;
+
   std::string render(unsigned width)const {
-    return content;
+    return reflow(width, content);
   }
 };
 
@@ -65,12 +63,29 @@ int main() {
 
   auto s = reflow(4, "12345-abcde-12345-ab");
 
+   auto s1 = Style(Color::White, Color::Green, Font::Crossed);
+   auto s2 = Style(
+       Color::Blue,
+       Color::White,
+       fonts(
+         Font::Underline,
+         Font::Crossed,
+         Font::Italic
+       ));
+
+   auto s3 = Style(
+       Color::Red,
+       Color::Blue,
+       fonts(
+         Font::Bold,
+        // Font::Crossed,
+         Font::Italic
+       ));
 
 
-  std::cout << std::endl <<s << std::endl;
-
-
-
+  std::cout << s1.render() <<"lol"<< "\e[0m"<<std::endl;
+  std::cout << s2.render() <<"lol"<< "\e[0m"<<std::endl;
+  std::cout << s1.render() << diff(s1, s3).render().substr(1) << std::endl;
   return 0;
 }
 
