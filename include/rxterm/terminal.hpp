b7e6752907ebccc3a2e0ef9349233f7cd361fc65
xokc <xokc@mail.ru>
2020-11-24 12:19:32 +0300

    Add MS Windows (MinGW) support

--- include/rxterm/terminal.hpp
+++ include/rxterm/terminal.hpp
@@ -1,10 +1,14 @@
 #ifndef RXTERM_TERMINAL_HPP
 #define RXTERM_TERMINAL_HPP
 
-
 #include <iostream>
 #include <algorithm>
 #include <string>
+
+#ifdef __WIN32
+#include <mutex>
+#endif
+
 #include <rxterm/utils.hpp>
 
 namespace rxterm {
@@ -27,8 +31,35 @@ struct VirtualTerminal {
     std::flush(std::cout);
     return {next};
   }
+
+#ifdef __WIN32
+  static std::once_flag initInstanceFlag;
+  static DWORD initWindowsTerminal() {
+#ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
+#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
+#endif
+  // Set output mode to handle virtual terminal sequences
+  HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  if (hOut == INVALID_HANDLE_VALUE) return GetLastError();
+  DWORD dwMode = 0;
+  if (!GetConsoleMode(hOut, &dwMode)) return GetLastError();
+  dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+  if (!SetConsoleMode(hOut, dwMode)) return GetLastError();
+  return ERROR_SUCCESS;
+}
+
+  VirtualTerminal() {
+    std::call_once(initInstanceFlag, VirtualTerminal::initWindowsTerminal);
+  }
+  VirtualTerminal(const VirtualTerminal&) = default;
+  VirtualTerminal& operator=(const VirtualTerminal&) = default;
+  VirtualTerminal(const std::string &buf) : buffer{buf} {};
+#endif
 };
 
+#ifdef __WIN32
+std::once_flag VirtualTerminal::initInstanceFlag;
+#endif
 }
 
 #endif
